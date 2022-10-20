e2f230e4e665c126d3df2e523bc3ce4994e74132
Tanvir Ahmad Arjel <tanvir.arjel@outlook.com>
2022-10-20 19:58:18 +0600

    Polly Context added to HttpRequestMessage

--- CallerApi/Controllers/WeatherForecastController.cs
+++ CallerApi/Controllers/WeatherForecastController.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using CallerApi.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Polly;
 using Polly.CircuitBreaker;
 
 namespace CallerApi.Controllers;
@@ -27,7 +29,17 @@ public class WeatherForecastController : ControllerBase
         try
         {
             HttpClient httpClient = _httpClientFactory.CreateClient("WeatherApi");
-            List<WeatherForecast> weatherForecasts = await httpClient.GetFromJsonAsync<List<WeatherForecast>>("WeatherForecast");
+
+            Context pollyContext = new Polly.Context().WithLogger<WeatherForecastController>(_logger);
+
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "WeatherForecast");
+            requestMessage.SetPolicyExecutionContext(pollyContext);
+
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(requestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            List<WeatherForecast> weatherForecasts = await httpResponseMessage.Content.ReadFromJsonAsync<List<WeatherForecast>>();
             return weatherForecasts;
         }
         catch (BrokenCircuitException)
